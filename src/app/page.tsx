a2a02cf3cf08242462826e59d2188ca2796238a9
saminoorsyed <saminoorsyed@gmail.com>
2024-12-12 10:26:41 -0500

    adjust slider positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-lexend)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[20px_1fr] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-lexend)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center">
         <article className="flex flex-col gap-8 row-start-2 items-center sm:items-start ">
           <h1 className="text-amberWhite text-8xl text-shadow-md"> Hi there!</h1>
           <p className="max-w-lg leading-relaxed text-lg">I'm a software developer based in New York City. I've put this site together to show off a bit of my work,and have a space to host some of the cool projects I'm building.</p>
--- src/components/ProjectSlider.tsx
+++ src/components/ProjectSlider.tsx
@@ -1,6 +1,64 @@
+import Image from "next/image"
+
+const projects = [
+  {
+    image: "/next.svg",
+    title: "some title1"
+  },
+  {
+    image: "/next.svg",
+    title: "some title2"
+  },
+  {
+    image: "/next.svg",
+    title: "some title3"
+  },
+  {
+    image: "/next.svg",
+    title: "some title4"
+  },
+  {
+    image: "/next.svg",
+    title: "some title5"
+  },
+  {
+    image: "/next.svg",
+    title: "some title6"
+  },
+  {
+    image: "/next.svg",
+    title: "some title7"
+  }
+]
+
 function ProjectSlider() {
+
   return (
-    <div></div>
+    <section>
+      <h2 className="text-amberDarker text-3xl">Projects</h2>
+      <article className="w-full flex justify-start ml-4 gap-2 flex-wrap p-4">
+        {...projects.map((project) => (
+          <div
+            key={project.title}
+            className="flex flex-col">
+            <p>{project.title}</p>
+            <div
+              className="border-amberDarker border-solid border shadow-lg p-4 flex-shrink-0"
+            >
+              <Image
+                src={project.image}
+                alt={project.title}
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
+
+        ))
+
+        }
+      </article>
+    </section>
   )
 }
 
